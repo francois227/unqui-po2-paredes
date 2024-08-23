7158246c8ebf7d0532ac7fb6a5931f4714088ef9
Franco Paredes <francoismaelparedes@hotmail.com>
2024-08-22 21:32:27 -0300

    commit base

---
+++ test/ar/edu/unq/po2/p3/CounterTestCase.java
@@ -0,0 +1,14 @@
+package ar.edu.unq.po2.p3;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CounterTestCase {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
